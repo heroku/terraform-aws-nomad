efb5fa062ffaa1bee2490985a16d938f45f5ad13
Cyril David <cyx@cyx.is>
2019-09-10 22:28:50 -0700

    us-east-1 setup

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/nomad-consul-ami/nomad-consul.json. If no AMI is specified, the template will 'just work' by using the example public AMIs. WARNING! Do not use the example AMIs in a production setting!"
   type        = string
-  default     = null
+  default     = "ami-03679e1c6e7585efb"
 }
 
 variable "cluster_name" {
